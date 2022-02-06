08588705ed532a2599a60c8893add35c29a4128f
Fauzi Fadhillah <fauzifadhi@gmail.com>
2022-02-06 16:57:56 +0700

    change database to postgres

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,13 +4,14 @@ import { Sequelize } from "sequelize-typescript";
 
 dotenv.config()
 
-const {DB_NAME, DB_PORT = 3306, DB_USERNAME, DB_PASSWORD} = process.env || {}
+const {DB_NAME, DB_PORT = 3306, DB_USER, DB_PASSWORD, DB_HOST} = process.env || {}
 const db = new Sequelize({
-  dialect: 'mysql',
+  dialect: 'postgres',
   database: DB_NAME,
   port: +DB_PORT,
-  username: DB_USERNAME,
+  username: DB_USER,
   password: DB_PASSWORD,
+  host: DB_HOST,
   define: {
     underscored: true,
   },
