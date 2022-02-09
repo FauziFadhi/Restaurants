a08b89db386f80bbd2338130ba5ac348eb21796e
Fauzi Fadhillah <fauzifadhi@gmail.com>
2022-02-09 17:23:18 +0700

    adjustment logic from elastic doc to object. - with groupby - with one object

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { SearchResponse } from 'elasticsearch';
+import { GetResponse, SearchResponse } from 'elasticsearch';
 
 export const ArrayToBulkElasticCreateDTO = <T = any>(
   index: string,
@@ -18,10 +18,36 @@ export const ArrayToBulkElasticCreateDTO = <T = any>(
   return elasticDTO;
 };
 
+export function elasticDocToObj<T>(data: GetResponse<T>): T & { id: string };
 export function elasticDocToObj<T>(
   data: Pick<SearchResponse<T>, 'hits'>,
-): (T & { id: string })[] {
-  return data.hits.hits.map((obj) => {
+  groupBy?: string[],
+): (T & { id: string })[];
+export function elasticDocToObj(...data: any[]) {
+  const datum = data[0];
+  if ('_id' in datum) {
+    return {
+      id: datum._id,
+      ...datum._source,
+    };
+  }
+
+  if (data[1].length >= 2 && typeof data[1][0] == 'string') {
+    const result = {};
+
+    data[1].forEach((index) => {
+      result[`${index}`] = [];
+    });
+    datum.hits.hits.forEach((obj) => {
+      result[`${obj._index}`]?.push({
+        id: obj._id,
+        ...obj._source,
+      });
+    });
+    return result;
+  }
+
+  return datum.hits.hits.map((obj) => {
     return {
       id: obj._id,
       ...obj._source,
